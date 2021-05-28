f304cc2ae721edf1427c6bd5d41d6788a1cbfe8a
khatabwedaa <khatabwedaa@gmail.com>
2021-05-28 13:56:27 +0200

    Update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ const Footer = (props: any) => (
     <div className="container px-6 mx-auto mt-16">
         <div className="flex flex-col items-center border-t border-gray-200">
             <div className="py-6 text-center sm:w-2/3">
-                <p className="text-sm text-gray-600">© {(new Date().getFullYear())} by Tailwind Components</p>
+                <p className="text-sm text-gray-600">© {(new Date().getFullYear())} by <a href="https://github.com/tailwindcomponents" target="_blank" rel="noopener noreferrer" className="text-primary hover:underline">Tailwind Components</a></p>
             </div>
         </div>
     </div>
