8ae55bfe4a90dc2bc36db225355495be4719c754
khatabwedaa <khatabwedaa@gmail.com>
2021-09-15 11:55:56 +0200

    Add missing Top / Right / Bottom / Left  classes fix #6

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = (props: any) => (
     <div className="container px-6 mx-auto mt-16">
-        <div className="flex flex-col items-center border-t border-gray-200">
+        <div className="flex flex-col items-center">
             <div className="py-6 text-center sm:w-2/3">
                 <p className="text-sm text-gray-600">© {(new Date().getFullYear())} by <a href="https://github.com/tailwindcomponents" target="_blank" rel="noopener noreferrer" className="text-primary hover:underline">Tailwind Components</a></p>
             </div>
