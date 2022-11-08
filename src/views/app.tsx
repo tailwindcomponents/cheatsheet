1f4ffb19cac7cf9380bbbacde178b88244e2e4f9
khatabwedaa <khatabwedaa@gmail.com>
2022-11-08 20:08:50 +0200

    Add google analytics & tag

--- src/views/app.tsx
+++ src/views/app.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from "react-router-dom";
 import Home from './home';
 import NoMatch from './no_match';
+import ReactGA from 'react-ga';
 
-const App = () => (
-    <Switch>
-        <Route exact path="/">
-            <Home />
-        </Route>
-        <Route path="*">
-            <NoMatch />
-        </Route>
-    </Switch>
-);
+class App extends Component {
+    setGA = () => {
+        ReactGA.initialize('UA-110182129-1');
+        ReactGA.pageview('Init page view');
+    };
+    
+    componentDidMount() {
+        this.setGA();
+    };
+
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="*">
+                    <NoMatch />
+                </Route>
+            </Switch>
+        )
+    }
+}
 
 export default App;
