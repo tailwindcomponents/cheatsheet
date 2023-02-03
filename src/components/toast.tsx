bbf9feaf96b0fec608503bab6c0bfc87556388a8
khatabwedaa <khatabwedaa@gmail.com>
2023-02-03 16:30:19 +0200

    Update toast component

--- src/components/toast.tsx
+++ src/components/toast.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-export const Toast = ({ text }: { text:string }) => {
+export const Toast = ({ text }: { text: string }) => {
 
     return (
-            <div
-                className="fixed top-1/4 left-1/3 p-4 m-4 bg-primary text-slate-900 text-white rounded-lg shadow-lg z-10"
-                role="alert"
-            >
-                <p className="text-sm">"{text}" copied into your clipboard </p>
-            </div>
+        <div
+            className="fixed z-10 p-4 m-4 font-medium text-white rounded-lg shadow-lg top-20 right-6 bg-primary"
+            role="alert"
+        >
+            <p className="text-sm">"{text}" copied into your clipboard </p>
+        </div>
     );
 };
