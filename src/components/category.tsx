71d8f91e46b9c1b4b38aaf4caaa255ddfda03c74
ilya smagin <ilya-z1999@ya.ru>
2023-06-18 15:01:49 +0300

    Removed opacity tracking No more needed as header is z-50 since #14

--- src/components/category.tsx
+++ src/components/category.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import Subcategory from '../modules/models/subcategory';
 import SubcategoryComponent from './subcategory';
-import classNames from "classnames";
 
 const Category = ({ category }: any) => {
-  let hasVisibleContent = category.content.filter((el: Subcategory) => el.table.length).length > 0
   return (
-    <div className={classNames("rounded-md bg-gray-100 dark:bg-gray-800 pt-4 m-2 overflow-hidden", {
-      "bg-opacity-20": !hasVisibleContent,
-    })}>
-      <h1 className={classNames("px-3 py-2 mx-3 mb-2 font-bold text-gray-800 dark:bg-gray-700 dark:text-gray-200 tracking-wider bg-gray-200 rounded-md", {
-        "bg-opacity-10": !hasVisibleContent,
-        "text-opacity-20": !hasVisibleContent
-      })}>{category.title}</h1>
+    <div className={"rounded-md bg-gray-100 dark:bg-gray-800 pt-4 m-2 overflow-hidden"}>
+      <h1 className={"px-3 py-2 mx-3 mb-2 font-bold text-gray-800 dark:bg-gray-700 dark:text-gray-200 tracking-wider bg-gray-200 rounded-md"}>
+        {category.title}
+      </h1>
       {
         category.content.map((subcategory: Subcategory, index: Number) => {
           return subcategory.table.length > 0 && <SubcategoryComponent key={'Subcat-' + index} subcategory={subcategory} />;
