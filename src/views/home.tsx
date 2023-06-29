c2234e3c92f776a353046a6aaa78014e8574d35d
ilya smagin <ilya-z1999@ya.ru>
2023-06-30 01:03:12 +0300

    [Feature] Search includes category name

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -20,34 +20,37 @@ const Home = (props: any) => {
 
     const search = (text: string) => {
         let newCheatsheet: Category[] = json.map((category: Category) => {
-            return {
-                // TODO try to add category table into considiration when searching. should reduce mapping
-                title: category.title,
-                content: category.content.map((subcategory: Subcategory) => {
-					// Si el texto de búsqueda existe en el título o la descripción se muestra toda la tabla, si no pues se filtra en ella
-					// If the search text exists in the title or description, the entire table is displayed, if not, then it is filtered on it
-					if (
-						subcategory.title.toLowerCase().includes(text) ||
-						subcategory.description.toLowerCase().includes(text)
-					) {
-						return subcategory;
-					} else {
-						return {
-							title: subcategory.title,
-							docs: subcategory.docs,
-							description: subcategory.description,
-							table: subcategory.table.filter((tr) => {
-								//no forEach needed as we need only one match to show entire row
-								for (let td = 0; td < tr.length; td++) {
-									if (tr[td].includes(text)) {
-										return true;
-									}
-								}
-								return false;
-							}),
-						};
-					}
-				}),
+            if (category.title.toLowerCase().includes(text)) {
+                return category;
+            } else {
+                return {
+                    title: category.title,
+                    content: category.content.map((subcategory: Subcategory) => {
+                        // Si el texto de búsqueda existe en el título o la descripción se muestra toda la tabla, si no pues se filtra en ella
+                        // If the search text exists in the title or description, the entire table is displayed, if not, then it is filtered on it
+                        if (
+                            subcategory.title.toLowerCase().includes(text) ||
+                            subcategory.description.toLowerCase().includes(text)
+                        ) {
+                            return subcategory;
+                        } else {
+                            return {
+                                title: subcategory.title,
+                                docs: subcategory.docs,
+                                description: subcategory.description,
+                                table: subcategory.table.filter((tr) => {
+                                    //no forEach needed as we need only one match to show entire row
+                                    for (let td = 0; td < tr.length; td++) {
+                                        if (tr[td].includes(text)) {
+                                            return true;
+                                        }
+                                    }
+                                    return false;
+                                }),
+                            };
+                        }
+                    })
+                }
             };
         });
 
